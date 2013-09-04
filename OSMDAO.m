dfcd6c18a060ba0a3300ff9d513330ac72d3b503
David Chiles <dwalterc@gmail.com>
2013-09-03 18:05:44 -0700

    fixed crash

--- OSMDAO.m
+++ OSMDAO.m
@@ -210,7 +210,7 @@
                 if (alreadyExists) {
                     [updateNodes addObject:node];
                 }
-                else
+                else if(node)
                 {
                     [newNodes addObject:node];
                 }
